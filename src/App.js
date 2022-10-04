bcc5ecd35291e8414598ab70eed28abe1c55aaf0
blackd44 <irabd44@gmail.com>
2022-10-04 02:59:50 +0200

    first practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import './App.scss';
+
+import image from './assets/01-Six-Elements-of-Modern-House-Design-You-Need-to-Know.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <div className='bars'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <ul className='links' >
+          <li><Link>HOME</Link></li>
+          <li><Link to={'/shop'} className='active'>SHOP</Link></li>
+          <li><Link>NEWS</Link></li>
+          <li><Link>CONTACT</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact path='/shop' element={
+            <>
+              <div className='content'>
+                <div>
+                  <div className='line'></div>
+                  <div className='title'>PURCHASE NOW.</div>
+                  <div className='summary'>A quite place for living.</div>
+                </div>
+                <div className='img'>
+                  <div className='button'>
+                    <div>buy now</div>
+                    <div> {'>'} </div>
+                  </div>
+                  <img alt="on Sale" src={image} />
+                </div>
+              </div>
+              <div className='bottomDiv'>
+                <div>Scroll Down</div>
+                <div>01/04</div>
+              </div>
+            </>
+          } />
+      </Routes>
+    </>
   );
 }
 
